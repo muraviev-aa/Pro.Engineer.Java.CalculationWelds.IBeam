43aa8db6937e076d2e900da8f97e78728d5d6cba
Aleksei Muraviev <muraviev-aa@yandex.ru>
2023-06-15 07:36:33 +0300

    Added MomentInertiaWeldRibsTwo

---
+++ src/main/java/ru/weld/MomentInertiaWeldRibsTwo.java
@@ -0,0 +1,43 @@
+package ru.weld;
+
+public class MomentInertiaWeldRibsTwo {
+//    private double lengthRibTwo;
+//    private double bevelRibTwo;
+//    private double thicknessRibTwo;
+//    private double sideRibTwo;
+//    private double factor;
+//    private double heightBeam;
+
+    public double lengthWeld(double lengthRibTwo) {
+        return lengthRibTwo - 1;
+    }
+
+    public double widthWeld(double sideRibTwo, double factor) {
+        return sideRibTwo * factor;
+    }
+
+    public double distanceCenterGravityWeldOverBelowX(double flangeWidth, double lengthRibTwo) {
+        return 0.5 * (flangeWidth + lengthRibTwo);
+    }
+
+    public double distanceCenterGravityWeldOverY(double heightBeam, double sideRibTwo, double factor) {
+        return 0.5 * (heightBeam + sideRibTwo * factor);
+    }
+
+    public double distanceCenterGravityWeldBelowY(double heightBeam, double thicknessRibTwo,
+                                                  double sideRibTwo, double factor) {
+        return 0.5 * heightBeam - thicknessRibTwo - 0.5 * sideRibTwo * factor;
+    }
+
+    public double momentInertiaWeldRibsTwoX(double heightBeam, double lengthRibTwo, double thicknessRibTwo,
+                                            double sideRibTwo, double factor) {
+        double l = lengthWeld(lengthRibTwo);
+        double w = widthWeld(sideRibTwo, factor);
+        double dOver = distanceCenterGravityWeldOverY(heightBeam, sideRibTwo, factor);
+        double dBelow = distanceCenterGravityWeldBelowY(heightBeam, thicknessRibTwo, sideRibTwo, factor);
+        double s = sideRibTwo * factor;
+        double iOverX = ((l * Math.pow(w, 3)) / 12) + Math.pow(dOver, 2) * s;
+        double iBelowX = ((l * Math.pow(w, 3)) / 12) + Math.pow(dBelow, 2) * s;
+        return 4 * (iOverX + iBelowX);
+    }
+}
---
+++ src/test/java/ru/weld/MomentInertiaWeldRibsTwoTest.java
@@ -0,0 +1,75 @@
+package ru.weld;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class MomentInertiaWeldRibsTwoTest {
+    @Test
+    void when70B4Then6Point75() {
+        double lengthRibTwo = 7.75;
+        double expected = 6.75;
+        MomentInertiaWeldRibsTwo momentInertiaWeldRibsTwo = new MomentInertiaWeldRibsTwo();
+        double out = momentInertiaWeldRibsTwo.lengthWeld(lengthRibTwo);
+        Assertions.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    void when70B4Then0Point6() {
+        double sideRibTwo = 0.6;
+        double factor = 1;
+        double expected = 0.6;
+        MomentInertiaWeldRibsTwo momentInertiaWeldRibsTwo = new MomentInertiaWeldRibsTwo();
+        double out = momentInertiaWeldRibsTwo.widthWeld(sideRibTwo, factor);
+        Assertions.assertEquals(expected, out, 0.1);
+    }
+
+    @Test
+    void when70B4Then16Point98() {
+        double flangeWidth = 26.2;
+        double lengthRibTwo = 7.75;
+        double expected = 16.98;
+        MomentInertiaWeldRibsTwo momentInertiaWeldRibsTwo = new MomentInertiaWeldRibsTwo();
+        double out = momentInertiaWeldRibsTwo.distanceCenterGravityWeldOverBelowX(flangeWidth, lengthRibTwo);
+        Assertions.assertEquals(expected, out, 0.01);
+    }
+
+    @Test
+    void when70B4Then35Point8() {
+        double heightBeam = 71;
+        double sideRibTwo = 0.6;
+        double factor = 1;
+        double expected = 35.8;
+        MomentInertiaWeldRibsTwo momentInertiaWeldRibsTwo = new MomentInertiaWeldRibsTwo();
+        double out = momentInertiaWeldRibsTwo.distanceCenterGravityWeldOverY(heightBeam, sideRibTwo, factor);
+        Assertions.assertEquals(expected, out, 0.1);
+    }
+
+    @Test
+    void when70B4Then34Point2() {
+        double heightBeam = 71;
+        double thicknessRibTwo = 1;
+        double sideRibTwo = 0.6;
+        double factor = 1;
+        double expected = 34.2;
+        MomentInertiaWeldRibsTwo momentInertiaWeldRibsTwo = new MomentInertiaWeldRibsTwo();
+        double out = momentInertiaWeldRibsTwo.distanceCenterGravityWeldBelowY(heightBeam, thicknessRibTwo,
+                sideRibTwo, factor);
+        Assertions.assertEquals(expected, out, 0.1);
+    }
+
+    @Test
+    void when70B4Then39711Point71() {
+//        double heightBeam = 71;
+//        double lengthRibTwo = 7.75;
+//        double thicknessRibTwo = 1;
+//        double sideRibTwo = 0.6;
+//        double factor = 1;
+//        double expected = 39711.71;
+//        MomentInertiaWeldRibsTwo momentInertiaWeldRibsTwo = new MomentInertiaWeldRibsTwo();
+//        double out = momentInertiaWeldRibsTwo.momentInertiaWeldRibsTwoX(heightBeam, lengthRibTwo,
+//                thicknessRibTwo, sideRibTwo, factor);
+//        Assertions.assertEquals(expected, out, 0.01);
+    }
+}
